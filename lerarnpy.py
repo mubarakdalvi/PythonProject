a93703f03d2d04d813ea326ba72b4fedf7a5447b
Mubarak Dalvi <127726167+mubarakdalvi@users.noreply.github.com>
2025-07-13 10:52:17 +0530

    added

---
+++ lerarnpy.py
@@ -0,0 +1,30 @@
+a = int(25)
+b = str('gd')
+c = float(25.00)
+d = bool(False)
+e = None
+f = complex(2j)
+g = range(20)
+hg = list(str(12))
+i = tuple((24,4))
+j = set((3245,54564))
+k = dict({"name": "Mub"})
+l = frozenset((14,451,5))
+
+# operators arithematic
+
+r1 = a * c
+r2 = a + c
+r3 = a - c
+r4 = a / c
+r5 = a % c
+
+print(r1,r2,r3,r4,r5)
+
+r6 = a == c
+r7 = a >= c
+r8 = a <= c
+r9 = a > c
+r10 = a < c
+r11 = a != c
+
